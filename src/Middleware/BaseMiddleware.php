e8aa15c5d1eb2ff64e38a9d184685874e6e9dbd6
Martín M <skydiver@users.noreply.github.com>
2020-06-02 00:43:58 -0300

    added dev dependencies, removed laravel requirment

--- src/Middleware/BaseMiddleware.php
+++ src/Middleware/BaseMiddleware.php
@@ -2,20 +2,17 @@
 
 namespace Skydiver\LaravelRouteBlocker\Middleware;
 
-use Closure;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Support\Str;
-
 class BaseMiddleware
 {
     public function handleNoAccess()
     {
-        // REDIRECT OR THROW ERROR
+        // REDIRECT IF NEEDED
         $redirect_to = config('laravel-route-blocker.redirect_to');
         if (!empty($redirect_to) && filter_var($redirect_to, FILTER_VALIDATE_URL)) {
             return redirect()->to($redirect_to);
         }
 
+        // THROW ERROR
         $response_status = config('laravel-route-blocker.response_status');
         $response_message = config('laravel-route-blocker.response_message');
         abort($response_status, $response_message);
